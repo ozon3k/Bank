aca221c8b9e7d5e027cc2a5242152127ba1b4cfd
Pawel Ozga <pawel.ozga@hotmail.com>
2018-07-21 13:16:54 +0200

    Programik zrobiony2

--- src/com/pawelozga/Main.java
+++ src/com/pawelozga/Main.java
@@ -3,14 +3,18 @@ package com.pawelozga;
 public class Main {
 
     public static void main(String[] args) {
-	Account Rachunek1 = new Firmowy("elo","1234567890",5000,"123aasd","345asd");
+        Account Rachunek1 = new Firmowy("elo", "1234567890", 5000, "123aasd", "345asd");
 
+        System.out.println(Rachunek1.dane);
+        System.out.println(Rachunek1.getType());
         System.out.println(Rachunek1.stanKonta);
 
-	Rachunek1.otwarcie();
+        Rachunek1.otwarcie();
         Rachunek1.wplata(30);
         System.out.println(Rachunek1.stanKonta);
         Rachunek1.wyplata(15);
         System.out.println(Rachunek1.stanKonta);
+        Rachunek1.zamkniecie();
+        Rachunek1.wyplata(1);
     }
 }
